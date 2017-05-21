707a7afae403dcdbd87f412cd52c5e7ad72a85ec
David Prause <praused@hotmail.com>
2017-05-21 09:43:33 -0400

    Completed example

--- Ch06Ex05_DelegateToCallFunction/Program.cs
+++ Ch06Ex05_DelegateToCallFunction/Program.cs
@@ -8,8 +8,34 @@ namespace Ch06Ex05_DelegateToCallFunction
 {
     class Program
     {
+        delegate double ProcessDelegate(double param1, double param2);
+
+        static double Multiply(double param1, double param2)
+        {
+            return param1 * param2;
+        }
+
+        static double Divide(double param1, double param2)
+        {
+            return param1 / param2;
+        }
+
         static void Main(string[] args)
         {
+            ProcessDelegate process;
+            Console.WriteLine("Enter two numbers separated with a comma:");
+            string input = Console.ReadLine();
+            int commaPos = input.IndexOf(',');
+            double param1 = Convert.ToDouble(input.Substring(0, commaPos));
+            double param2 = Convert.ToDouble(input.Substring(commaPos + 1, input.Length - commaPos - 1));
+            Console.WriteLine("Enter M to multiply or D to divide:");
+            input = Console.ReadLine();
+            if (input.ToUpper() == "M")
+                process = new ProcessDelegate(Multiply);
+            else
+                process = new ProcessDelegate(Divide);
+            Console.WriteLine($"Result: {process(param1, param2)}");
+            Console.ReadKey();
         }
     }
 }
